c7195961acff1e4188210bb2cc4587ab3d382d2d
左翔太朗 <s.hidaridesu@gmail.com>
2024-06-26 08:58:55 +0900

    chore(deps): setup for web.

---
+++ app/entry.client.tsx
@@ -0,0 +1,13 @@
+import { RemixBrowser } from "@remix-run/react";
+import { StrictMode, startTransition } from "react";
+import { hydrateRoot } from "react-dom/client";
+const app = document.querySelector("#app");
+if (!app) throw new Error("#app not found.");
+startTransition(() => {
+	hydrateRoot(
+		app,
+		<StrictMode>
+			<RemixBrowser />
+		</StrictMode>,
+	);
+});
---
+++ app/root.tsx
@@ -0,0 +1,18 @@
+import { Outlet, Scripts } from "@remix-run/react";
+export function HydrateFallback() {
+	return (
+		<>
+			<p>Loading...</p>
+			<Scripts />
+		</>
+	);
+}
+
+export default function Component() {
+	return (
+		<>
+			<Outlet />
+			<Scripts />
+		</>
+	);
+}
